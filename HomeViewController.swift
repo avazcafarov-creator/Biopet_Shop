ad0565e77e9bb2a691263b9cea563c9234fa4d8a
avazcafarov-creator <avazcafarov@gmail.com>
2026-08-22 15:11:46 +0400

    search bar
    view-lar constraint add ele

--- HomeViewController.swift
+++ HomeViewController.swift
@@ -8,19 +8,73 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    private var searchBar: UISearchBar = {
-       let searchBar = UISearchBar()
-        searchBar.placeholder = "Search"
-        searchBar.searchBarStyle = .minimal
+//    private var searchBar: UISearchBar = {
+//       let searchBar = UISearchBar()
+//        searchBar.placeholder = "Royal Canin, Sanicat, qurd dərmanı..."
+//        searchBar.searchTextField.font = .systemFont(ofSize: 12, weight: .regular)
+//        searchBar.searchBarStyle = .minimal
+//        searchBar.backgroundImage = UIImage()
+//        searchBar.searchTextField.backgroundColor = .whiteBiopet
+//        searchBar.searchTextField.layer.cornerRadius = 8
+//        searchBar.translatesAutoresizingMaskIntoConstraints = false
+//        return searchBar
+//    }()
+    private lazy var profileIcon: UIImageView = {
+        let profileIcon = UIImageView()
+        profileIcon.image = UIImage(named: "user_filled")?.withRenderingMode(.alwaysTemplate)
+        profileIcon.tintColor = .red
+        profileIcon.backgroundColor = .neuralGray
+        profileIcon.layer.borderWidth = 3
+        profileIcon.layer.borderColor = UIColor.neuralGray.cgColor
+        profileIcon.layer.cornerRadius = 16
+        profileIcon.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        profileIcon.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        profileIcon.translatesAutoresizingMaskIntoConstraints = false
+        return profileIcon
+    }()
+    
+    private lazy var searchBar: UIView = {
+        let searchBar = UIView()
+        searchBar.backgroundColor = .whiteBiopet
+        searchBar.layer.cornerRadius = 8
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         return searchBar
+    }()
+    
+    private lazy var searchBarImageView: UIImageView = {
+        let searchBarImageView = UIImageView()
+        searchBarImageView.contentMode = .scaleAspectFit
+        searchBarImageView.tintColor = .neuralGray
+        searchBarImageView.image = UIImage(systemName: "search")
+        searchBarImageView.translatesAutoresizingMaskIntoConstraints = false
+        return searchBarImageView
+    }()
+    
+    private lazy var searchBarTextField: UITextField = {
+        let searchBarTextField = UITextField()
+        searchBarTextField.placeholder = "Royal Canin, Sanicat, qurd dərmanı..."
+        searchBarTextField.font = .systemFont(ofSize: 12, weight: .regular)
+        searchBarTextField.textColor = .neuralGray
+        searchBarTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchBarTextField
+    }()
+    
+    private lazy var notificationButton: UIButton = {
+        let button = UIButton()
+        button.setImage(.notification, for: .normal)
+        button.backgroundColor = .whiteBiopet
+        button.layer.cornerRadius = 8
+        button.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     private lazy var categoryCollectionView: UICollectionView = {
        let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
-        layout.minimumLineSpacing = 8
-        layout.minimumInteritemSpacing = 8
+        layout.minimumLineSpacing = 12
+        layout.minimumInteritemSpacing = 12
         
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.register(CategoryCell.self, forCellWithReuseIdentifier: "CategoryCell")
@@ -41,11 +95,15 @@ class HomeViewController: UIViewController {
     
     private func configureUI() {
         view.backgroundColor = .white
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Profile", image: .init(systemName: "person.crop.circle.fill"), target: self, action: #selector(profileButtonTapped))
     }
     
     private func configureConstraints() {
+        view.addSubview(profileIcon)
         
+        NSLayoutConstraint.activate([
+            profileIcon.topAnchor.constraint(equalTo: view.topAnchor, constant: 64),
+            profileIcon.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+        ])
     }
     
     @objc private func profileButtonTapped() {
